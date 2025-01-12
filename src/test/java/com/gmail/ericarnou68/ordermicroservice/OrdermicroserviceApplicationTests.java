fd90d89699ae05e0ed1fb160b34ff3ea22767d12
Eric Arnou Santos <ericarnou68@gmail.com>
2025-01-12 00:41:12 -0300

    initial files and enviromeant

---
+++ src/test/java/com/gmail/ericarnou68/ordermicroservice/OrdermicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.ericarnou68.ordermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
